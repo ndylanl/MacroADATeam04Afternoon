2584ab9e61b4391c672292af33245c2b893cccf9
ndylanl <dylanlienardi@gmail.com>
2024-10-08 17:35:04 +0700

    feat, added reference to colors

---
+++ MacroAfternoon04/History/HistoryMonthCircle.swift
@@ -0,0 +1,18 @@
+//
+//  HistoryMonthCircle.swift
+//  MacroAfternoon04
+//
+//  Created by Nicholas Dylan Lienardi on 08/10/24.
+//
+
+import SwiftUI
+
+struct HistoryMonthCircle: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HistoryMonthCircle()
+}
---
+++ MacroAfternoon04/History/HistoryWeekCircle.swift
@@ -0,0 +1,19 @@
+//
+//  HistoryWeekCircle.swift
+//  MacroAfternoon04
+//
+//  Created by Nicholas Dylan Lienardi on 08/10/24.
+//
+
+import SwiftUI
+
+struct HistoryWeekCircle: View {
+    var body: some View {
+        Circle()
+            .fill(Color.theme.primaryColorTheme)
+    }
+}
+
+#Preview {
+    HistoryWeekCircle()
+}

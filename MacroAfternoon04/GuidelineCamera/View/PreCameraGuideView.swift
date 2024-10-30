43286c1b015c3d7f0158642d0d121bf197edbc5b
ndylanl <dylanlienardi@gmail.com>
2024-10-30 18:37:07 +0700

    feat, heatmap sudah bisa ditampilkan

---
+++ MacroAfternoon04/GuidelineCamera/View/PreCameraGuideView.swift
@@ -0,0 +1,18 @@
+//
+//  PreCameraGuideView.swift
+//  MacroAfternoon04
+//
+//  Created by Nicholas Dylan Lienardi on 30/10/24.
+//
+
+import SwiftUI
+
+struct PreCameraGuideView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PreCameraGuideView()
+}
---
+++ MacroAfternoon04/Topography/TopographyView.swift
@@ -0,0 +1,18 @@
+//
+//  TopographyView.swift
+//  MacroAfternoon04
+//
+//  Created by Nicholas Dylan Lienardi on 28/10/24.
+//
+
+import SwiftUI
+
+struct TopographyView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TopographyView()
+}

a77273212b66cc9ad06b790abe6c4357da5baee7
anin <banindya10@gmail.com>
2024-11-01 15:11:22 +0700

    chore, change dashboard ui and layout feat, add daily tips on dashboard

---
+++ MacroAfternoon04/Dashboard/View/DailyTipsView.swift
@@ -0,0 +1,18 @@
+//
+//  DailyTipsView.swift
+//  MacroAfternoon04
+//
+//  Created by Benedikta Anin on 31/10/24.
+//
+
+import SwiftUI
+
+struct DailyTipsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DailyTipsView()
+}

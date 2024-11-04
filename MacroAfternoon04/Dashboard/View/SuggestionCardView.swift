a98c1927d67e36ce9756f6ef58e9853e6cdf04ce
anin <banindya10@gmail.com>
2024-11-04 16:46:37 +0700

    feat add daily tips, create suggestion card

---
+++ MacroAfternoon04/Dashboard/View/SuggestionCardView.swift
@@ -0,0 +1,18 @@
+//
+//  SuggestionCardView.swift
+//  MacroAfternoon04
+//
+//  Created by Benedikta Anin on 01/11/24.
+//
+
+import SwiftUI
+
+struct SuggestionCardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SuggestionCardView()
+}

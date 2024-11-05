247629d373aa18e04dfcbab2b4f72b4d682dd12e
ndylanl <dylanlienardi@gmail.com>
2024-11-05 16:08:54 +0700

    feat, added guide camera on where users take photos and change status retry text color.

---
+++ MacroAfternoon04/Tracking/View/CameraScalpPositionGuideView.swift
@@ -0,0 +1,18 @@
+//
+//  CameraScalpPositionGuideView.swift
+//  MacroAfternoon04
+//
+//  Created by Nicholas Dylan Lienardi on 05/11/24.
+//
+
+import SwiftUI
+
+struct CameraScalpPositionGuideView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CameraScalpPositionGuideView()
+}
